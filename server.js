34446508ff51ff0e72edb692fd1d978b2a47864e
Kelsea <andersonkelsea@gmail.com>
2016-06-11 11:58:20 -0400

    Added top tracks to related artist search

--- server.js
+++ server.js
@@ -7,37 +7,67 @@ app.use(express.static('public'));
 var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
-           .qs(args)
-           .end(function(response) {
-                if (response.ok) {
-                    emitter.emit('end', response.body);
-                }
-                else {
-                    emitter.emit('error', response.code);
-                }
-            });
+    .qs(args)
+    .end(function(response) {
+        emitter.emit('end', response.body);
+    });
     return emitter;
 };
 
+var getTracks = function(artist, cb){
+    unirest.get('https://api.spotify.com/v1/artists/' + artist.id + '/top-tracks?country=US')
+    .end(function(response){
+        if(!response.error){
+            artist.tracks = response.body.tracks;
+            cb();
+        } else{
+            cb(response.error);
+        }
+    });
+};
+
 app.get('/search/:name', function(req, res) {
-    var searchReq = getFromApi('search', {
-        q: req.params.name,
-        limit: 1,
-        type: 'artist'
-    });
+  var searchReq = getFromApi('search', {
+    q: req.params.name,
+    limit: 1,
+    type: 'artist'
+});
 
-    searchReq.on('end', function(item) {
-        var artist = item.artists.items[0];
-        var relatedArtist = getFromApi('artists/' + artist.id + '/related-artists');
-        relatedArtist.on('end', function getRelated(artistArray){
-            artist.related = artistArray.artists;
-        res.json(artist);
-        });
-    });
+  searchReq.on('end', function(item) {
+      var artist = item.artists.items[0];
+      unirest.get('https://api.spotify.com/v1/artists/' + artist.id + '/related-artists')
+      .end(function(response) {
+          if (!response.error) {
+              artist.related = response.body.artists;
 
-    searchReq.on('error', function(code) {
-        res.sendStatus(code);
-    });
+              var totalArtists = artist.related.length;
+              var completed = 0;
+
+              var checkComplete = function() {
+                  if (completed === totalArtists) {
+                      res.json(artist);
+                  }
+              };
+
+              artist.related.forEach(function(artist) {
+                  getTracks(artist, function(err) {
+                      if (err) {
+                          res.sendStatus(code);
+                      }
+                      completed += 1;
+                      checkComplete();
+                  });
+              });
+
+          } else {
+              res.sendStatus(code);
+          }
+      });    
+  });
+
+  searchReq.on('error', function() {
+      res.sendStatus(code);
+  });
 });
 
 app.listen(8080);
